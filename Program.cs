c9634d17a7fd81dadd40a60c9520aeb537ef5724
Harol Cisneros <harolcsnrs@gmail.com>
2024-11-08 16:01:54 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using Proyecto_Colas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_pilas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            cola cola = null;
+            int opcion = 0;
+            while(opcion != 6)
+            {
+                Console.WriteLine("1.- Establecer Tamaño");
+                Console.WriteLine("2.- Imprimir conteo");
+                Console.WriteLine("3.- Insert");
+                Console.WriteLine("4.- Extract");
+                Console.WriteLine("5.- Imprimir Cola");
+                Console.WriteLine("6.- Salir");
+                opcion =Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Usted eligió la opción 1");
+                        Console.WriteLine("Ingresa el tamaño de la cola");
+                        int max=Convert.ToInt32(Console.ReadLine());
+                        cola = new cola(max);
+                        break;
+
+                        case 2:
+
+                        if(cola != null)
+                        {
+                            Console.WriteLine("Usted eligió la opción 2");
+                            int n = cola.Count();
+                            if(n != 0)
+                            {
+                                Console.WriteLine($"El numero de elemento insertado es {n}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("La cola esta vacía");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Esta vacía");
+                        }
+                        break;
+
+                    case 3:
+                        if(cola != null)
+                        {
+                            Console.WriteLine("Usted eligió la opción 3");
+                            Console.WriteLine("Dame valor del Nodo");
+                            int num = Convert.ToInt32(Console.ReadLine());
+                            cola.Insert(num);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Esta vacía");
+                        }
+                        break;
+
+                        case 4:
+                        if(cola != null)
+                        {
+                            Console.WriteLine("Usted eligió la opción 4");
+                            int valorsacado=cola.Extract();
+                            if(valorsacado != -1)
+                            {
+                                Console.WriteLine($"El valor sacado es {valorsacado}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("La cola esta Underflow");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Esta vacía");
+                        }
+                        break;
+
+                    case 5:
+                        if(cola != null)
+                        {
+                            Console.WriteLine("Usted eligió la opción 5");
+                            cola.Print();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Esta vacía");
+                        }
+                        break;
+
+
+                    case 6:
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ cola.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Colas
+{
+    internal class cola
+    {
+        private int MAX;
+        private int count = 0;
+        private nodo inicio;
+
+        public cola (int max)
+        {
+            MAX = max;
+            inicio = null;
+        }
+
+        private bool Underflow()
+        {
+            if (inicio == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool Overflow()
+        {
+            if(MAX==count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Insert(int num)
+        {
+            //Regresa true si se inserto exitosamente
+            //regresa false si la cola esta lleno
+            nodo nuevo = new nodo(num);
+            if (Overflow())
+            {
+                Console.WriteLine("La cola esta Overflow");
+                return false;
+            }
+            else if (Underflow())
+            {
+                inicio = nuevo;
+                count++;
+                return true;
+            }
+            else
+            {
+                nodo act = inicio;
+                while (act.Sig != null)
+                {
+                    act = act.Sig;
+                }
+                act.Sig = nuevo;
+                count++;
+                return true;
+            }
+        }
+        public int Extract()
+        {
+            //regresa el numero sacado de la cola 0
+            //regresa -1 cuando la cola esta vacia
+            if (Underflow())
+            {
+                return -1;
+            }
+            else
+            {
+                int valorsacado = inicio.Valor;
+                inicio = inicio.Sig;
+                count--;
+                return valorsacado;
+            }
+        }
+        public int Count(int n=0)
+        {
+            //regresa cuantos elementos tiene la cola
+            nodo act;
+            act = inicio;
+            if (Underflow())
+            {
+                return n;
+            }
+            else
+            {
+                n++;
+                while(act.Sig != null)
+                {
+                    act= act.Sig;
+
+                    n++;
+                }
+                return n;
+            }
+        }
+        public void Print()
+        {
+            //imprime la cola
+            nodo act;
+            act = inicio;
+            if (act == null)
+            {
+                Console.WriteLine("La pila esta Underflow");
+            }
+            else
+            {
+                while (act != null)
+                {
+                    Console.WriteLine($"[{act.Valor}]");
+                    act = act.Sig;
+                }
+            }
+        }
+    }
+}
